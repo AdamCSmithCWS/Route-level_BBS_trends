735fb07c1bb8e8b95044de2b41edd192ee24b16e
Smith <Adam.Smith@ec.gc.ca>
2023-06-09 09:53:25 -0400

    RUFU example

---
+++ 13_Fit_RUHU_covariate_example.R
@@ -0,0 +1,537 @@
+
+library(bbsBayes2)
+library(tidyverse)
+library(sf)
+library(cmdstanr)
+library(patchwork)
+
+output_dir <- "output"
+species <- "Rufous Hummingbird" 
+
+
+species_f <- gsub(gsub(species,pattern = " ",replacement = "_",fixed = T),pattern = "'",replacement = "",fixed = T)
+
+spp <- "_habitat_"
+
+exp_t <- function(x){
+  y <- (exp(x)-1)*100
+}
+
+firstYear <- 2006
+lastYear <- 2021
+
+
+out_base <- paste0(species_f,spp,firstYear,"_",lastYear)
+
+
+
+
+sp_data_file <- paste0("Data/",species_f,"_",firstYear,"_",lastYear,"_covariate_stan_data.RData")
+
+
+load(sp_data_file)
+
+mod.file = paste0("models/slope",spp,"route_NB.stan")
+
+# trend habitat effects are not changed, but the intercept effect is
+# removes the optional spatial components for intercepts 
+stan_data[["fit_spatial"]] <- 0 # this sets an option in the model
+# to estimate the residual intercept component using a simple random
+# effect, instead of a spatial one. This allows the model to estimate
+# variation in abundance that is not predicted by local habitat suitability
+# but does not fit an inherently spatial residual structure
+# setting this fit_spatial value to 1 uses the iCAR structure to model
+# a spatially explicit residual term
+
+
+
+slope_model <- cmdstan_model(mod.file, stanc_options = list("Oexperimental"))
+
+
+
+stanfit <- slope_model$sample(
+  data=stan_data,
+  refresh=400,
+  iter_sampling=2000,
+  iter_warmup=2000,
+  parallel_chains = 4)
+
+summ <- stanfit$summary()
+print(paste(species, stanfit$time()[["total"]]))
+
+saveRDS(stanfit,
+        paste0(output_dir,"/",out_base,"_stanfit.rds"))
+
+saveRDS(summ,
+        paste0(output_dir,"/",out_base,"_summ_fit.rds"))
+
+summ %>% arrange(-rhat)
+
+
+
+
+
+# graphing ----------------------------------------------------------------
+
+
+firstYear <- 2006
+lastYear <- 2021
+
+out_base <- paste0(species_f,spp,firstYear,"_",lastYear)
+
+sp_data_file <- paste0("Data/",species_f,"_",firstYear,"_",lastYear,"_covariate_stan_data.RData")
+
+load(sp_data_file)
+
+summ <- readRDS(paste0(output_dir,"/",out_base,"_summ_fit.rds"))
+
+
+# mn0 <- new_data %>% 
+#   group_by(routeF) %>% 
+#   summarise(mn = mean(count),
+#             mx = max(count),
+#             ny = n(),
+#             fy = min(year),
+#             ly = max(year),
+#             sp = max(year)-min(year))
+
+route_map_2006 <- route_map 
+
+exp_t <- function(x){
+  y <- (exp(x)-1)*100
+}
+
+
+# plot trends -------------------------------------------------------------
+
+
+base_strata_map <- bbsBayes2::load_map("bbs_usgs")
+
+
+strata_bounds <- st_union(route_map) #union to provide a simple border of the realised strata
+bb = st_bbox(strata_bounds)
+xlms = as.numeric(c(bb$xmin,bb$xmax))
+ylms = as.numeric(c(bb$ymin,bb$ymax))
+
+betas1 <- summ %>% 
+  filter(grepl("beta[",variable,fixed = TRUE)) %>% 
+  mutate(across(2:7,~exp_t(.x)),
+         routeF = as.integer(str_extract(variable,"[[:digit:]]{1,}")),
+         parameter = "Full with Habitat-Change") %>% 
+  select(routeF,mean,sd,parameter) %>% 
+  rename(trend = mean,
+         trend_se = sd)
+
+alpha1 <- summ %>% 
+  filter(grepl("alpha[",variable,fixed = TRUE)) %>% 
+  mutate(across(2:7,~exp(.x)),
+         routeF = as.integer(str_extract(variable,"[[:digit:]]{1,}")),
+         parameter = "Full with Habitat") %>% 
+  select(routeF,median,sd) %>% 
+  rename(abundance = median,
+         abundance_se = sd)
+betas1 <- betas1 %>% 
+  inner_join(.,alpha1)
+
+alpha2 <- summ %>% 
+  filter(grepl("alpha_resid[",variable,fixed = TRUE)) %>% 
+  mutate(across(2:7,~exp(.x)),
+         routeF = as.integer(str_extract(variable,"[[:digit:]]{1,}")),
+         parameter = "Residual") %>% 
+  select(routeF,median,sd) %>% 
+  rename(abundance = median,
+         abundance_se = sd)
+
+
+betas2 <- summ %>% 
+  filter(grepl("beta_resid[",variable,fixed = TRUE)) %>% 
+  mutate(across(2:7,~exp_t(.x)),
+         routeF = as.integer(str_extract(variable,"[[:digit:]]{1,}")),
+         parameter = "Residual") %>% 
+  select(routeF,mean,sd,parameter) %>% 
+  rename(trend = mean,
+         trend_se = sd)
+
+betas2 <- betas2 %>% 
+  inner_join(.,alpha2,by = "routeF") 
+
+
+betas <- bind_rows(betas1,betas2)
+
+plot_map <- route_map_2006 %>% 
+  left_join(.,betas,
+            by = "routeF",
+            multiple = "all") 
+
+breaks <- c(-7, -4, -2, -1, -0.5, 0.5, 1, 2, 4, 7)
+lgnd_head <- "Mean Trend\n"
+trend_title <- "Mean Trend"
+labls = c(paste0("< ",breaks[1]),paste0(breaks[-c(length(breaks))],":", breaks[-c(1)]),paste0("> ",breaks[length(breaks)]))
+labls = paste0(labls, " %/year")
+plot_map$Tplot <- cut(plot_map$trend,breaks = c(-Inf, breaks, Inf),labels = labls)
+
+
+map_palette <- c("#a50026", "#d73027", "#f46d43", "#fdae61", "#fee090", "#ffffbf",
+                 "#e0f3f8", "#abd9e9", "#74add1", "#4575b4", "#313695")
+names(map_palette) <- labls
+
+
+
+map <- ggplot()+
+  geom_sf(data = base_strata_map,
+          fill = NA,
+          colour = grey(0.75))+
+  geom_sf(data = plot_map,
+          aes(colour = Tplot,
+              size = abundance))+
+  scale_size_continuous(range = c(0.05,2),
+                        name = "Mean Count")+
+  scale_colour_manual(values = map_palette, aesthetics = c("colour"),
+                      guide = guide_legend(reverse=TRUE),
+                      name = paste0(lgnd_head))+
+  coord_sf(xlim = xlms,ylim = ylms)+
+  theme_bw()+
+  guides(size = "none")+
+  labs(title = paste(firstYear,"-",lastYear))+
+  facet_wrap(vars(parameter))
+
+
+map_abund <- ggplot()+
+  geom_sf(data = base_strata_map,
+          fill = NA,
+          colour = grey(0.75))+
+  geom_sf(data = plot_map,
+          aes(colour = abundance))+
+  scale_colour_viridis_c(begin = 0.1, end = 0.9,
+                         guide = guide_legend(reverse=TRUE),
+                         name = paste0("Relative Abundance"))+
+  coord_sf(xlim = xlms,ylim = ylms)+
+  theme_bw()+
+  labs(title = paste(firstYear,"-",lastYear))+
+  facet_wrap(vars(parameter))
+
+
+#map
+
+# pdf(paste0("Figures/Four_trends_model_comparison_",species_f,".pdf"),
+#     height = 8,
+#     width = 8)
+# print(map)
+# dev.off()
+
+map_se <- ggplot()+
+  geom_sf(data = base_strata_map,
+          fill = NA,
+          colour = grey(0.75))+
+  geom_sf(data = plot_map,
+          aes(colour = trend_se,
+              size = abundance_se))+
+  scale_size_continuous(range = c(0.05,2),
+                        name = "SE of Mean Count",
+                        trans = "reverse")+
+  scale_colour_viridis_c(aesthetics = c("colour"),
+                         guide = guide_legend(reverse=TRUE),
+                         name = paste0("SE of Trend"))+
+  coord_sf(xlim = xlms,ylim = ylms)+
+  theme_bw()+
+  guides(size = "none")+
+  labs(title = paste(firstYear,"-",lastYear))+
+  facet_wrap(vars(parameter))
+
+
+
+
+#print(map2 / map_se2)
+
+# pdf(paste0("Figures/Figure_supplement_1_Trend_map_w_habitat_and_withing_",species_f,".pdf"),
+#     height = 10.5,
+#     width = 7.5)
+# 
+# 
+# print(map / map_se + plot_layout(guides = "collect"))
+# 
+# 
+# dev.off()
+
+map_save <- map / map_abund + plot_layout(guides = "collect")
+saveRDS(map_save,paste0("Figures/saved_map_RUHU_covariate_",firstYear,".rds"))
+
+pdf(paste0("Figures/Figure_8.pdf"),
+    height = 5,
+    width = 7)
+
+
+print(map)
+
+
+dev.off()
+
+pdf(paste0("Figures/Figure_8_alt.pdf"),
+    height = 10,
+    width = 7)
+
+
+print(map / map_abund + plot_layout(guides = "collect"))
+
+
+dev.off()
+
+  
+#   
+# 
+# 
+# firstYear <- 1985
+# lastYear <- ifelse(firstYear == 1985,2005,2021)
+# 
+# out_base <- paste0(species_f,spp,firstYear,"_",lastYear)
+# 
+# sp_data_file <- paste0("Data/",species_f,"_",firstYear,"_",lastYear,"_stan_data.RData")
+# 
+# load(sp_data_file)
+# 
+# summ <- readRDS(paste0(output_dir,"/",out_base,"_summ_fit.rds"))
+# 
+# 
+# 
+# mn0 <- new_data %>% 
+#   group_by(routeF) %>% 
+#   summarise(mn = mean(count),
+#             mx = max(count),
+#             ny = n(),
+#             fy = min(year),
+#             ly = max(year),
+#             sp = max(year)-min(year))
+# 
+# route_map_2006 <- route_map 
+# 
+# exp_t <- function(x){
+#   y <- (exp(x)-1)*100
+# }
+# 
+# 
+# # plot trends -------------------------------------------------------------
+# 
+# 
+# base_strata_map <- bbsBayes2::load_map("bbs_usgs")
+# 
+# 
+# strata_bounds <- st_union(route_map) #union to provide a simple border of the realised strata
+# bb = st_bbox(strata_bounds)
+# xlms = as.numeric(c(bb$xmin,bb$xmax))
+# ylms = as.numeric(c(bb$ymin,bb$ymax))
+# 
+# betas1 <- summ %>% 
+#   filter(grepl("beta[",variable,fixed = TRUE)) %>% 
+#   mutate(across(2:7,~exp_t(.x)),
+#          routeF = as.integer(str_extract(variable,"[[:digit:]]{1,}")),
+#          parameter = "full") %>% 
+#   select(routeF,mean,sd,parameter) %>% 
+#   rename(trend = mean,
+#          trend_se = sd)
+# 
+# 
+# alpha1 <- summ %>% 
+#   filter(grepl("alpha[",variable,fixed = TRUE)) %>% 
+#   mutate(across(2:7,~exp(.x)),
+#          routeF = as.integer(str_extract(variable,"[[:digit:]]{1,}")),
+#          parameter = "full") %>% 
+#   select(routeF,median,sd) %>% 
+#   rename(abundance = median,
+#          abundance_se = sd)
+# 
+# alpha2 <- summ %>% 
+#   filter(grepl("alpha_resid[",variable,fixed = TRUE)) %>% 
+#   mutate(across(2:7,~exp(.x)),
+#          routeF = as.integer(str_extract(variable,"[[:digit:]]{1,}")),
+#          parameter = "Residual") %>% 
+#   select(routeF,median,sd) %>% 
+#   rename(abundance = median,
+#          abundance_se = sd)
+# 
+# betas1 <- betas1 %>% 
+#   inner_join(.,alpha1)
+# 
+# betas2 <- summ %>% 
+#   filter(grepl("beta_trend[",variable,fixed = TRUE)) %>% 
+#   mutate(across(2:7,~exp_t(.x)),
+#          routeF = as.integer(str_extract(variable,"[[:digit:]]{1,}")),
+#          parameter = "Residual") %>% 
+#   select(routeF,mean,sd,parameter) %>% 
+#   rename(trend = mean,
+#          trend_se = sd)
+# betas2 <- betas2 %>% 
+#   inner_join(.,alpha2,by = "routeF") %>% 
+#   inner_join(.,mn0,by = "routeF")
+# 
+# 
+# betas <- bind_rows(betas1,betas2)
+# 
+# plot_map <- route_map_2006 %>% 
+#   left_join(.,betas,
+#             by = "routeF",
+#             multiple = "all") 
+# 
+# breaks <- c(-7, -4, -2, -1, -0.5, 0.5, 1, 2, 4, 7)
+# lgnd_head <- "Mean Trend\n"
+# trend_title <- "Mean Trend"
+# labls = c(paste0("< ",breaks[1]),paste0(breaks[-c(length(breaks))],":", breaks[-c(1)]),paste0("> ",breaks[length(breaks)]))
+# labls = paste0(labls, " %/year")
+# plot_map$Tplot <- cut(plot_map$trend,breaks = c(-Inf, breaks, Inf),labels = labls)
+# 
+# 
+# map_palette <- c("#a50026", "#d73027", "#f46d43", "#fdae61", "#fee090", "#ffffbf",
+#                  "#e0f3f8", "#abd9e9", "#74add1", "#4575b4", "#313695")
+# names(map_palette) <- labls
+# 
+# 
+# 
+# map <- ggplot()+
+#   geom_sf(data = base_strata_map,
+#           fill = NA,
+#           colour = grey(0.75))+
+#   geom_sf(data = plot_map,
+#           aes(colour = Tplot,
+#               size = abundance))+
+#   scale_size_continuous(range = c(0.05,2),
+#                         name = "Mean Count")+
+#   scale_colour_manual(values = map_palette, aesthetics = c("colour"),
+#                       guide = guide_legend(reverse=TRUE),
+#                       name = paste0(lgnd_head))+
+#   coord_sf(xlim = xlms,ylim = ylms)+
+#   theme_bw()+
+#   guides(size = "none")+
+#   labs(title = paste(firstYear,"-",lastYear))+
+#   facet_wrap(vars(parameter))
+# 
+# 
+# 
+# map_abund <- ggplot()+
+#   geom_sf(data = base_strata_map,
+#           fill = NA,
+#           colour = grey(0.75))+
+#   geom_sf(data = plot_map,
+#           aes(colour = abundance))+
+#   scale_colour_viridis_c(begin = 0.1, end = 0.9,
+#                          guide = guide_legend(reverse=TRUE),
+#                          name = paste0("Relative Abundance"))+
+#   coord_sf(xlim = xlms,ylim = ylms)+
+#   theme_bw()+
+#   labs(title = paste(firstYear,"-",lastYear))+
+#   facet_wrap(vars(parameter))
+# 
+# 
+# #map
+# 
+# # pdf(paste0("Figures/Four_trends_model_comparison_",species_f,".pdf"),
+# #     height = 8,
+# #     width = 8)
+# # print(map)
+# # dev.off()
+# 
+# map_se <- ggplot()+
+#   geom_sf(data = base_strata_map,
+#           fill = NA,
+#           colour = grey(0.75))+
+#   geom_sf(data = plot_map,
+#           aes(colour = trend_se,
+#               size = abundance_se))+
+#   scale_size_continuous(range = c(0.05,2),
+#                         name = "SE of Mean Count",
+#                         trans = "reverse")+
+#   scale_colour_viridis_c(aesthetics = c("colour"),
+#                          guide = guide_legend(reverse=TRUE),
+#                          name = paste0("SE of Trend"))+
+#   coord_sf(xlim = xlms,ylim = ylms)+
+#   theme_bw()+
+#   guides(size = "none")+
+#   labs(title = paste(firstYear,"-",lastYear))+
+#   facet_wrap(vars(parameter))
+# 
+# 
+# 
+# 
+# #print(map2 / map_se2)
+# 
+# pdf(paste0("Figures/Figure_supplement_1_1985_Trend_map_w_habitat_and_without_",species_f,".pdf"),
+#     height = 10.5,
+#     width = 7.5)
+# 
+# 
+# print(map / map_se + plot_layout(guides = "collect"))
+# 
+# 
+# dev.off()
+# 
+# map_save <- map / map_abund + plot_layout(guides = "collect")
+# saveRDS(map_save,paste0("Figures/saved_map_",firstYear,".rds"))
+# pdf(paste0("Figures/Figure_4both_1985.pdf"),
+#     height = 10,
+#     width = 7)
+# 
+# 
+# print(map / map_abund + plot_layout(guides = "collect"))
+# 
+# 
+# dev.off()
+# 
+# 
+# # summarise the parameters ------------------------------------------------
+# 
+# hypers_out <- NULL
+# route_params_out <- NULL
+# 
+# 
+# for(firstYear in c(1985,2006)){
+# lastYear <- ifelse(firstYear == 1985,2005,2021)
+# 
+# out_base <- paste0(species_f,spp,firstYear,"_",lastYear)
+# 
+# sp_data_file <- paste0("Data/",species_f,"_",firstYear,"_",lastYear,"_stan_data.RData")
+# 
+# load(sp_data_file)
+# 
+# summ <- readRDS(paste0(output_dir,"/",out_base,"_summ_fit.rds"))
+# 
+# 
+# betahabs <- summ %>% 
+#   filter(grepl("beta_hab[",variable,fixed = TRUE))  %>% 
+#   mutate(across(2:7,~exp_t(.x)),
+#          firstyear = firstYear,
+#          parameter = "Rho_beta_r")
+# alphahabs <- summ %>% 
+#   filter(grepl("alpha_hab[",variable,fixed = TRUE))  %>% 
+#   mutate(across(2:7,~exp(.x)),
+#          firstyear = firstYear,
+#          parameter = "Rho_alpha_r")
+# route_params_out <- bind_rows(route_params_out,betahabs)
+# route_params_out <- bind_rows(route_params_out,alphahabs)
+# 
+# 
+# Bhabs <- summ %>% 
+#   filter(grepl("BETA_hab",variable,fixed = TRUE)) %>% 
+#   mutate(firstyear = firstYear,
+#          parameter = "Rho_beta")
+# Ahabs <- summ %>% 
+#   filter(grepl("ALPHA_hab",variable,fixed = TRUE)) %>% 
+#   mutate(firstyear = firstYear,
+#          parameter = "Rho_alpha")
+# CH <- summ %>% 
+#   filter(variable == "CH") %>% 
+#   mutate(firstyear = firstYear)
+# TT <- summ %>% 
+#   filter(variable %in% c("T","T_no_habitat")) %>% 
+#   mutate(firstyear = firstYear)
+# hypers_out <- bind_rows(hypers_out,Bhabs)
+# hypers_out <- bind_rows(hypers_out,Ahabs)
+# hypers_out <- bind_rows(hypers_out,CH)
+# hypers_out <- bind_rows(hypers_out,TT)
+# 
+# }
+# 
+# saveRDS(hypers_out,"saved_hyperparameters.rds")
+# saveRDS(route_params_out,"saved_route_parameters.rds")
+# 
+
+
+
+  
---
+++ models/slope_habitat_route_NB.stan
@@ -0,0 +1,209 @@
+// This is a Stan implementation of a route-level slope model
+// plus, it has an explicitly spatial prior structure on the 
+// random effect, and covariates on the intercepts and trends
+
+//iCAR function
+ functions {
+   real icar_normal_lpdf(vector bb, int ns, array[] int n1, array[] int n2) {
+     return -0.5 * dot_self(bb[n1] - bb[n2])
+       + normal_lpdf(sum(bb) | 0, 0.001 * ns); //soft sum to zero constraint on bb
+  }
+ }
+
+
+data {
+  int<lower=1> nroutes;
+  int<lower=1> ncounts;
+  int<lower=1> nyears;
+  int<lower=1> nobservers;
+ 
+  array [ncounts] int<lower=0> count;   // count observations
+  array [ncounts] int<lower=1> year; // year index
+  array [ncounts] int<lower=1> route; // route index
+  array [ncounts] int<lower=0> firstyr; // first year index =1 if observer's first year on route, 0 otherwise
+  array [ncounts] int<lower=1> observer;   // observer indicators
+  
+  // mean annual habitat suitability on route (centered and scaled)
+  array [nroutes] real route_habitat;              
+  // mean rate of change in annual habitat suitability on route (centered)
+  array [nroutes] real route_habitat_slope;              
+
+  int<lower=1> fixedyear; // centering value for years
+ 
+ // spatial neighbourhood information
+  int<lower=1> N_edges;
+  array [N_edges] int<lower=1, upper=nroutes> node1;  // node1[i] adjacent to node2[i]
+  array [N_edges] int<lower=1, upper=nroutes> node2;  // and node1[i] < node2[i]
+
+  int<lower=0, upper=1> fit_spatial; //if 1 then use spatial component to model intercept residual
+  // if 0 then use simple random effect
+
+}
+
+parameters {
+
+  vector[nroutes] beta_raw;// non-centered residual trend
+  real BETA; // hyperparameter mean residual trend
+  vector[nroutes] rho_beta_raw_hab;// non-centered habitat-based trend
+  real rho_BETA_hab; // hyperparameter mean habitat-based trend
+
+  vector[nroutes] alpha_raw;// non-centered residual intercept
+  real ALPHA; // hyperparameter mean residual intercept
+  vector[nroutes] rho_alpha_raw_hab;// non-centered habitat-based intercept
+  real rho_ALPHA_hab; // hyperparameter mean habitat-based intercept
+
+  real eta; //first-year intercept
+  
+  vector[nobservers] obs_raw; //observer effects
+
+  real<lower=0> sdnoise;    // inverse of sd of over-dispersion
+  real<lower=0> sdobs;    // sd of observer effects
+  real<lower=0> sdbeta;    // sd of residual slopes 
+  real<lower=0> sdrho_beta_hab;    // sd of habitat-change effect on slopes 
+  real<lower=0> sdalpha;    // sd of residual intercepts
+  real<lower=0> sdrho_alpha_hab;    // sd of habitat effect on intercepts
+
+  
+}
+
+transformed parameters{
+  
+   vector[nroutes] beta; // full slope
+   vector[nroutes] beta_resid; //residual component of slope
+   vector[nroutes] beta_hab; //habitat component of slope
+   
+  vector[nroutes] alpha; // full intercept
+  vector[nroutes] alpha_hab; //habitat component intercepts
+  vector[nroutes] alpha_resid; // residual component intercepts
+  vector[nobservers] obs; // observer effects
+  real phi;//dipsersion of negative binomial
+  vector[ncounts] E;           // predicted log-scale counts (lambda)
+
+
+// covariate effect on intercepts and slopes
+ 
+   beta_resid = (sdbeta*beta_raw) + BETA;
+   alpha_resid = (sdalpha*alpha_raw) + ALPHA;
+   
+   for(s in 1:nroutes){
+   beta_hab[s] = ((sdrho_beta_hab*rho_beta_raw_hab[s]) + rho_BETA_hab) * (route_habitat_slope[s]);
+   alpha_hab[s] = ((sdrho_alpha_hab*rho_alpha_raw_hab[s]) + rho_ALPHA_hab) * (route_habitat[s]);
+   }
+   
+   alpha =  alpha_resid + alpha_hab;
+   beta = beta_resid  + beta_hab;
+  
+   
+   obs = sdobs*obs_raw;
+
+  phi = 1/sqrt(sdnoise); //as recommended to avoid prior that places most prior mass at very high overdispersion by https://github.com/stan-dev/stan/wiki/Prior-Choice-Recommendations
+
+  for(i in 1:ncounts){
+    E[i] =  alpha[route[i]] + beta[route[i]] * (year[i]-fixedyear)  + obs[observer[i]] + eta*firstyr[i];
+  }
+  
+  
+}
+
+model {
+
+
+
+
+  // habitat-change effects on slope
+  rho_beta_raw_hab ~  normal(0,1); 
+  sum(rho_beta_raw_hab) ~ normal(0,0.001*nroutes);//soft zero-sum constraint
+  // habitat-change effects on slope
+  rho_alpha_raw_hab ~ normal(0,1);
+  sum(rho_alpha_raw_hab) ~ normal(0,0.001*nroutes);//soft zero-sum constraint
+
+  
+    // spatially varying residual trend and intercepts
+  beta_raw ~ icar_normal(nroutes, node1, node2); 
+  if(fit_spatial){
+   alpha_raw ~ icar_normal(nroutes, node1, node2);
+  }else{
+  alpha_raw ~ normal(0,1);
+  sum(alpha_raw) ~ normal(0,0.001*nroutes);
+  }
+
+  
+  sdnoise ~ normal(0,0.5); //prior on scale of extra Poisson log-normal variance
+
+  sdobs ~ normal(0,0.3); //prior on sd of gam hyperparameters
+ 
+  obs_raw ~ normal(0,1);//observer effects
+  sum(obs_raw) ~ normal(0,0.001*nobservers); //soft-zero-sum constraint
+
+  BETA ~ normal(0,0.1);// prior on fixed effect mean slope
+  rho_BETA_hab ~ normal(0,0.1);// prior on haibtat-change effect on slope
+  ALPHA ~ normal(0,1);// prior on fixed effect mean intercept
+  rho_ALPHA_hab ~ normal(0,1);// prior on habitat effect on intercept
+  eta ~ normal(0,1);// prior on first-year observer effect
+  
+  
+
+  sdalpha ~ normal(0,1); //prior on sd of intercept 
+  sdrho_alpha_hab ~ normal(0,1); //prior on sd of habitat effect on intercept 
+
+  sdbeta ~ normal(0,0.1);// prior on sd of slope spatial variation w mean = 0.04 and 99% < 0.13
+  sdrho_beta_hab ~ normal(0,0.1);// prior on sd of habitat-change effect on slope 
+
+
+  // full count likelihood
+  count ~ neg_binomial_2_log(E,phi); //vectorized count likelihood with log-transformation
+ 
+
+
+}
+
+ generated quantities {
+
+
+   array[nroutes,nyears] real<lower=0> nsmooth;
+   array[nroutes,nyears] real<lower=0> nsmooth_no_habitat;
+    array[nyears] real<lower=0> NSmooth;
+    array[nyears] real<lower=0> NSmooth_no_habitat;
+    real T; // full end-point trend of slope-based trajectory
+    real T_no_habitat; // end-point trend of slope-based trajectory without habitat-component of slope
+    real CH; // total change transformation of trend 
+    real CH_no_habitat; //total change transformation of trend without habitat-component of slope
+    real CH_dif; // difference between CH and CH_no_habitat
+    real T_dif; // difference between T and T_no
+    real retrans_obs = 0;// 0.5*(sdobs^2);
+
+// intercepts and slopes
+
+
+ for(y in 1:nyears){
+
+      for(s in 1:nroutes){
+
+
+
+      nsmooth[s,y] = exp(alpha[s] + beta[s] * (y-fixedyear)  + retrans_obs);//
+      nsmooth_no_habitat[s,y] = exp(alpha[s] + beta_resid[s] * (y-fixedyear)  + retrans_obs);//
+        }
+  NSmooth[y] = mean(nsmooth[,y]);
+  NSmooth_no_habitat[y] = mean(nsmooth_no_habitat[,y]);
+  
+    }
+    T = 100*(((NSmooth[nyears]/NSmooth[1])^(1.0/nyears))-1);
+    
+    
+    T_no_habitat = 100*(((NSmooth_no_habitat[nyears]/NSmooth_no_habitat[1])^(1.0/nyears))-1);
+    
+        CH = 100*((NSmooth[nyears]/NSmooth[1])-1);
+    
+    
+    CH_no_habitat = 100*((NSmooth_no_habitat[nyears]/NSmooth_no_habitat[1])-1);
+    
+    CH_dif = CH-CH_no_habitat;
+    T_dif = T-T_no_habitat;
+    
+  }
+
+
+
+ 
+
